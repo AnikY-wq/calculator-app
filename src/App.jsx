26baebc9edf7229a610eca9eb35a6d1a89f85bb4
Vaishnavi Mahale <mahalevaishnavi001@gmail.com>
2024-06-19 16:58:24 +0530

    feat: redux implementation - add onclick functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,61 @@
 // import Button from "./components/Button/Button";
 // import ButtonBox from "./components/ButtonBox/ButtonBox";
 
-import Wrapper from "./components/Wrapper/Wrapper.jsx"
-import Screen from "./components/Screen/Screen.jsx"
+import Wrapper from "./components/Wrapper/Wrapper.jsx";
+import Screen from "./components/Screen/Screen.jsx";
 import Button from "./components/Button/Button.jsx";
 import ButtonBox from "./components/ButtonBox/ButtonBox.jsx";
+import { useSelector, useDispatch } from "react-redux";
 
 import btnValues from "./utils/buttonArray.js";
+import {
+  clearNumber,
+  currentNumber,
+  emptyNumber,
+} from "./feature/number/numberSlice.jsx";
+import { evaluatePostfix } from "./utils/calculatorLogic.js";
+import { useState } from "react";
 
 const App = () => {
-    return (
-        <Wrapper>
-            <Screen value="0" />
-            <ButtonBox>
-                {
-                    btnValues.flat().map((btn, i) => {
-                        return (
-                            <Button
-                                key={i}
-                                className={btn === "=" ? "equals" : ""}
-                                value={btn}
-                                onClick={() => {
-                                    console.log(`${btn} clicked!`);
-                                }}
-                            />
-                        )
-                    })
-                }
-            </ButtonBox>
-        </Wrapper>
-    );
+  const number = useSelector(state => state.number.value);
+  let isOperator = ["( )", "%", "/", "*", "-", "+", "."];
+  const [firstClicked, setFirstClicked] = useState(true);
+  const dispatch = useDispatch();
+  const handleButtonClick = btn => {
+    if (btn === "C") {
+      setFirstClicked(true);
+      return dispatch(clearNumber());
+    }
+    if (btn === "=") {
+      const res = evaluatePostfix(number);
+      return dispatch(currentNumber(Number(res)));
+    } else {
+      if (firstClicked) {
+        setFirstClicked(false);
+        dispatch(emptyNumber());
+      }
+      return dispatch(
+        currentNumber(!isOperator.includes(btn) ? Number(btn) : btn)
+      );
+    }
+  };
+  return (
+    <Wrapper>
+      <Screen value={number} />
+      <ButtonBox>
+        {btnValues.flat().map((btn, i) => {
+          return (
+            <Button
+              key={i}
+              className={btn === "=" ? "equals" : ""}
+              value={btn}
+              onClick={() => handleButtonClick(btn)}
+            />
+          );
+        })}
+      </ButtonBox>
+    </Wrapper>
+  );
 };
 
 export default App;
